e56e86e3027bb9840c2d13627449e25e17b0fe77
Admin <admin@admins-macbook-pro.local>
2018-04-21 19:54:37 -0700

    Simon Says part-02 added button alpha logic

---
+++ Simon Says/Circular Button.swift
@@ -0,0 +1,28 @@
+//
+//  Circular Button.swift
+//  Simon Says
+//
+//  Created by Admin on 4/21/18.
+//  Copyright © 2018 Matthew Heinrichs. All rights reserved.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.width/2
+        layer.masksToBounds = true
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                alpha = 1.0
+            } else {
+                alpha = 0.5
+            }
+        }
+    }
+
+}
